04f6c48ecec16fdc4e9448a1ee8efffe1b6c4e3d
Kristjan Krizman <kristjan.krizman@gmail.com>
2026-01-04 21:41:15 +0100

    refactor: plugin settings moved to main.lua

--- main.lua
+++ main.lua
@@ -26,7 +26,6 @@ local Size = require("ui/size")
 
 local _ = require("gettext")
 local T = require("ffi/util").template
-local Utils = require("utils")
 
 local FocusMode = InputContainer:new({
     name = "focus_mode",
@@ -38,10 +37,6 @@ local FocusMode = InputContainer:new({
     focus_mode_dialog = nil, -- reference to the main dialog instance
     debug_html_dialog = nil, -- reference to the debug HTML dialog instance
 })
-
-FocusMode.readSettings = Utils.readSettings
-FocusMode.readSubSetting = Utils.readSubSetting
-FocusMode.saveSubSetting = Utils.saveSubSetting
 
 local FOCUS_MODE_DIR = T("%1/plugins/%2.koplugin/", DataStorage:getDataDir(), FocusMode.name)
 local META_FILE_PATH = FOCUS_MODE_DIR .. "_meta.lua"
@@ -914,6 +909,41 @@ function FocusMode:onShowFocusModeHtmlOld(touchmenu_instance)
     self.debug_html_dialog = debug_dialog
     UIManager:show(self.debug_html_dialog)
     return true
+end
+
+function FocusMode:readSettings()
+    local default_settings = {
+        enabled = false,
+        from_time = { hour = 6, min = 0 },
+        to_time = { hour = 19, min = 0 },
+    }
+
+    local current_settings = self.settings:readSetting("focus_mode")
+
+    if not current_settings then
+        -- Settings don't exist, save the defaults
+        self.settings:saveSetting("focus_mode", default_settings)
+        self.settings:flush() -- write to disk immediately
+        return default_settings
+    end
+
+    return current_settings
+end
+
+-- Helper to read a specific key from the focus_mode sub-dictionary
+function FocusMode:readSubSetting(key)
+    logger.info("FocusMode: Reading %1", key)
+    local settings = self:readSettings()
+    return settings[key]
+end
+
+-- Helper to save a specific key into the focus_mode sub-dictionary
+function FocusMode:saveSubSetting(key, value)
+    logger.info("FocusMode: Saving %1: %2", key, value)
+    local settings = self:readSettings()
+    settings[key] = value
+    self.settings:saveSetting("focus_mode", settings)
+    self.settings:flush()
 end
 
 return FocusMode
